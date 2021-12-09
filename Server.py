805560be1c4a3e94d8f4e61f13671ab5847acab9
kingsalmonella <84696396+kingsalmonella@users.noreply.github.com>
2021-12-08 23:52:31 -0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,5 +1,7 @@
 import os
+
 import paramiko
+from paramiko import SSHClient, AutoAddPolicy
 class Server:
 
     """ Server class for representing and manipulating servers. """
@@ -12,3 +14,19 @@ class Server:
         # TODO - Use os module to ping the server
 
         return os.system('ping ' + self.server_ip)
+    def paramiko(self):
+        #Launch SSH
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(self.server_ip, 22, 'ec2-user')
+        #installing update by executing command
+        stdin, stdout, stderr = ssh.exec_command('sudo yum update')
+        for line in stdout.read().splitlines():
+            print(line)
+        #Installing upgrade by executing command
+        stdin, stdout, stderr = ssh.exec_command('sudo yum upgrade')
+        for line in stdout.read().splitlines():
+            print(line)
+
+        ssh.close()
